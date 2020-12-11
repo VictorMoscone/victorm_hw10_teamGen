f6408f7f213400de2f45b05c89d829e1cd6a97e1
Victor Moscone <victormoscone@gmail.com>
2020-12-10 22:07:11 -0800

    Refactor: Added all of my comments to app.js.

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+// This finds our directory labelled "output" and creates team.html when called.
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// Our global and empty array of employees for the page.
 const employeeBatch = [];
 
+// This inquirer prompt chain runs when the app is loaded since managers are required and are singular.
 const managerPrompt = () => {
     return inquirer.prompt([
         {
@@ -42,12 +45,16 @@ const managerPrompt = () => {
         }
     ])
     .then(answers => {
+        // This will create a new object with all the entered data.
         const managerGen = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOffNum);
+        // The object is then pushed into the employee batch array.
         employeeBatch.push(managerGen);
+        // The user is then asked if they want to add another employee.
         newEmpLogic(answers.addNew);
     });
 };
 
+// This inquirer prompt chain is ran when the user wants to add an Engineer.
 const engineerPrompt = () => {
     return inquirer.prompt([
         {
@@ -78,8 +85,11 @@ const engineerPrompt = () => {
         }
     ])
     .then(answers => {
+        // This will create a new object with all the entered data.
         const engineerGen = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engGitHub);
+        // The object is then pushed into the employee batch array.
         employeeBatch.push(engineerGen);
+        // The user is then asked if they want to add another employee.
         newEmpLogic(answers.addNew);
     });
 };
@@ -114,19 +124,29 @@ const internPrompt = () => {
         }
     ])
     .then(answers => {
+        // This will create a new object with all the entered data.
         const internGen = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+        // The object is then pushed into the employee batch array.
         employeeBatch.push(internGen);
+        // The user is then asked if they want to add another employee.
         newEmpLogic(answers.addNew);
     });
 };
 
+// This runs when the user selects in a prompt that they want to add another employee.
 const newEmpLogic = (answers) => {
+    // If they chose to add an Engineer...
     if (answers == "Engineer") {
+        // Run the Engineer inquirer prompts.
         engineerPrompt();
+    // If they chose to add an Intern...
     } else if (answers == "Intern") {
+        // Run the Intern inquirer prompts.
         internPrompt();
     } else {
+        // Otherwise, this is our sole end point.
         console.log("You're done!");
+        // We create the team.html using all of the employee objects in the employeeBatch array.
         fs.writeFile(outputPath, render(employeeBatch), (err) => {
             if (err) throw err;
             console.log("File Written.");
@@ -134,4 +154,5 @@ const newEmpLogic = (answers) => {
     };
 };
 
+// Start point.
 managerPrompt();
